4390b318df2ff3ccfbe538ff1bed3b7dcf3edc0e
happyntec <tec@happyn.cc>
2022-02-18 17:56:57 +0800

    HPYCLIENT-54

---
+++ Casks/tuntap.rb
@@ -0,0 +1,24 @@
+cask "tuntap" do
+  name "TunTap"
+  desc "happyn.cn tuntap for macos "
+  version "20150118"
+  homepage "happyn.cn"
+  url "https://github.com/happynclient/happynmacos/releases/download/0.2/happynet-tuntap.tar.gz"
+  sha256 "2f31ffece5ad349aaae0516c08e2f6bd569d61ba2ec54b27909ba71537e7b4ff"
+
+  pkg "tuntap_#{version}.pkg"
+
+  uninstall pkgutil:   "tuntap.startup_item.tap|tuntap.startup_item.tun|tuntap.tap|tuntap.tun",
+            launchctl: [
+              "net.sf.tuntaposx.tun.plist",
+              "net.sf.tuntaposx.tap.plist",
+            ],
+            kext:      [
+              "net.sf.tuntaposx.tap",
+              "net.sf.tuntaposx.tun",
+            ],
+            delete:    [
+              "/Library/LaunchDaemons/net.sf.tuntaposx.tap.plist",
+              "/Library/LaunchDaemons/net.sf.tuntaposx.tun.plist",
+            ]
+end
